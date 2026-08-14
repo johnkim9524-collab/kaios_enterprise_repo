99a277d3848550ffef3ff7f18b7698e16e71a8b0
johnkim9524-collab <john.kim9524@gmail.com>
2026-08-14 12:15:48 +0900

    feat(kidults): assemble normalized V658 robot visual

---
+++ apps/kidults-enterprise-staging/public/portal/components/assets/k100-toys-v658.js
@@ -0,0 +1,5 @@
+import part1 from "./k100-toys-v658-part1.js";
+import part2 from "./k100-toys-v658-part2.js";
+import part3 from "./k100-toys-v658-part3.js";
+
+export default [part1, part2, part3].join("");
