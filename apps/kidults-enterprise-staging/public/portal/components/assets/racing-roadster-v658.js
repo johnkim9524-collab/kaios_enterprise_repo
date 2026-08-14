13fcefda455707999979f2ad199539203058dd5a
johnkim9524-collab <john.kim9524@gmail.com>
2026-08-14 11:57:22 +0900

    feat(kidults): assemble V658 roadster master

---
+++ apps/kidults-enterprise-staging/public/portal/components/assets/racing-roadster-v658.js
@@ -0,0 +1,5 @@
+import part1 from "./racing-roadster-v658-part1.js";
+import part2 from "./racing-roadster-v658-part2.js";
+import part3 from "./racing-roadster-v658-part3.js";
+
+export default [part1, part2, part3].join("");
