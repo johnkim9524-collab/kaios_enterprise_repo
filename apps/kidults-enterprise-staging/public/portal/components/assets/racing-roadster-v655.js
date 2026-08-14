bc654fb8879ed61ad5a2a0a0ea8ff8a5edc203ba
johnkim9524-collab <john.kim9524@gmail.com>
2026-08-14 11:28:19 +0900

    fix(kidults): cache-bust V658 Hero asset parts

--- apps/kidults-enterprise-staging/public/portal/components/assets/racing-roadster-v655.js
+++ apps/kidults-enterprise-staging/public/portal/components/assets/racing-roadster-v655.js
@@ -1,6 +1,6 @@
-import part1 from "./racing-roadster-v655-part1.js";
-import part2 from "./racing-roadster-v655-part2.js";
-import part3a from "./racing-roadster-v655-part3a.js";
-import part3b from "./racing-roadster-v655-part3b.js";
+import part1 from "./racing-roadster-v655-part1.js?v=658";
+import part2 from "./racing-roadster-v655-part2.js?v=658";
+import part3a from "./racing-roadster-v655-part3a.js?v=658";
+import part3b from "./racing-roadster-v655-part3b.js?v=658";
 
 export default [part1, part2, part3a, part3b].join("");
